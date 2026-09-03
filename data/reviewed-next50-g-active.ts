2fd04ed257c2373d23504c7a498e4ff2c9e1aee3
turki-015-oss <304760497+turki-015-oss@users.noreply.github.com>
2026-09-03 19:08:07 +0300

    Add seventh reviewed batch of 50 common verbs

---
+++ data/reviewed-next50-g-active.ts
@@ -0,0 +1,7 @@
+export const REVIEWED_NEXT50_G_ACTIVE = [
+  "deviner","libérer","rater","engager","respirer","survivre","mériter","jurer","récupérer","pardonner",
+  "pousser","profiter","rejoindre","attaquer","prouver","réaliser","embrasser","sauter","danser","ressentir",
+  "traiter","mener","fonctionner","approcher","forcer","épouser","prévoir","dégager","souhaiter","surveiller",
+  "risquer","signifier","retirer","tenter","régler","diriger","échapper","remonter","ravir","brûler",
+  "crier","obliger","rêver","remarquer","traverser","charger","appartenir","répéter","reposer","plaisanter"
+] as const;
